fcc4005c9d99fa5daab5f2184fc4d7004d9d6417
Tauqeer-AriF <chtauqeer1117@gmail.com>
2021-05-21 00:27:30 -0700

    adobe page

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+
+function index1() {
+   window.open("index.html", "_self");
+}
+
+function index2() {
+    window.open("pages/index2.html", "_self");
+  }
+ 
+  function index3() {
+    window.open("pages/index3.html", "_self");
+  }
+
+  function index4() {
+    window.open("pages/index4.html", "_self");
+  }
+
+  function index5() {
+    window.open("pages/index5.html", "_self");
+  }
+
+
+  function index1_1() {
+    window.open("index.html", "_self");
+ }
+ 
+ function index1_2() {
+     window.open("index2.html", "_self");
+   }
+  
+   function index1_3() {
+     window.open("index3.html", "_self");
+   }
+ 
+   function index1_4() {
+     window.open("index4.html", "_self");
+   }
+ 
+   function index1_5() {
+     window.open("index5.html", "_self");
+   }
